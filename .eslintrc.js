6eb8f5cbb515b66c3f4e17fbdb9729a355fb8678
Andrew Huth <ahuth@chanzuckerberg.com>
2022-11-21 10:04:03 -0500

    Use @chanzuckerberg eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,10 @@
 /** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
-  extends: [
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-    "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
-  ],
   env: {
+    node: true,
+    es6: true,
     "cypress/globals": true,
   },
-  plugins: ["cypress"],
   // We're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but we have to
   // set the jest version explicitly.
@@ -18,4 +13,24 @@ module.exports = {
       version: 28,
     },
   },
+  plugins: ["@chanzuckerberg/eslint-plugin-edu-react", "cypress", "prettier"],
+  extends: [
+    "@chanzuckerberg/eslint-config-edu-js",
+    "@chanzuckerberg/eslint-config-edu-ts",
+    "@remix-run/eslint-config/jest-testing-library",
+    "plugin:@chanzuckerberg/eslint-plugin-edu-react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["storybook-static/", "build/"],
+  overrides: [
+    {
+      files: ["**/routes/**/*.js?(x)", "**/routes/**/*.tsx"],
+      rules: {
+        // Routes may use default exports without a name. At the route level
+        // identifying components for debugging purposes is less of an issue, as
+        // the route boundary is more easily identifiable.
+        "react/display-name": "off",
+      },
+    },
+  ],
 };
