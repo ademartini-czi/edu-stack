27e84194d515a84413de91bb4d62069805093960
Andrew Huth <ahuth@chanzuckerberg.com>
2023-01-09 08:53:39 -0500

    Use GraphQL

--- app/models/note.server.ts
+++ app/models/note.server.ts
@@ -7,28 +7,6 @@ export type Note = {
   body: string;
 };
 
-export async function getNote({
-  id,
-  userId,
-}: Pick<Note, 'id'> & {
-  userId: User['id'];
-}) {
-  const response = await fetch(`https://example.com/notes/${id}`);
-  const data = (await response.json()) as Note;
-
-  if (data.userId !== userId) {
-    return Promise.reject();
-  }
-
-  return data;
-}
-
-export async function getNoteListItems({userId}: {userId: User['id']}) {
-  const response = await fetch(`https://example.com/users/${userId}/notes`);
-  const data = (await response.json()) as Note[];
-  return data;
-}
-
 export async function createNote({
   body,
   title,
@@ -44,13 +22,4 @@ export async function createNote({
   const data = (await response.json()) as Note;
 
   return data;
-}
-
-export async function deleteNote({
-  id,
-  userId,
-}: Pick<Note, 'id'> & {userId: User['id']}) {
-  await fetch(`https://example.com/notes/${id}`, {
-    method: 'DELETE',
-  });
 }
