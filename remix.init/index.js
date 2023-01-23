9a35d077bf7a66106e4e3d2170f43768f5d6b3bc
Andrew Huth <ahuth@chanzuckerberg.com>
2023-01-23 08:49:41 -0500

    Use the `node:` prefix for node module imports in remix.init

--- remix.init/index.js
+++ remix.init/index.js
@@ -1,6 +1,6 @@
-const crypto = require('crypto');
-const fs = require('fs/promises');
-const path = require('path');
+const crypto = require('node:crypto');
+const fs = require('node:fs/promises');
+const path = require('node:path');
 const PackageJson = require('@npmcli/package-json');
 const {dedent} = require('ts-dedent');
 
