21ba244868add4691758b5df4784ed6adba60403
Andrew Huth <ahuth@chanzuckerberg.com>
2023-01-30 16:10:34 -0500

    Add explanatory comments in test setup

--- tests/setupTests.ts
+++ tests/setupTests.ts
@@ -2,5 +2,9 @@ import {installGlobals} from '@remix-run/node';
 import dotenv from 'dotenv';
 import '@testing-library/jest-dom';
 
+// Ensures globals such as "fetch", "Response", "Request", and "Headers" are present. These are
+// needed by Remix.
 installGlobals();
-dotenv.config(); // Ensure `process.env` variables are defined
+
+// Ensure env variables defined in .env are present during tests.
+dotenv.config();
