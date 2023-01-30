7e0dc5bcdb0315c1b5b104363f7caee0b051922a
Andrew Huth <ahuth@chanzuckerberg.com>
2023-01-30 16:06:39 -0500

    Tweak wording

--- jest.config.ts
+++ jest.config.ts
@@ -20,7 +20,7 @@ const config: JestConfigWithTsJest = {
     // Ensure we get CJS builds of packages with conditional exports, not ESM or browser builds.
     // Overrides jest-environment-jsdom's default value of 'browser' (see https://github.com/facebook/jest/blob/836157f4807893bb23a4758a60998fbd61cb184c/packages/jest-environment-jsdom/src/index.ts#L40).
     // Without this, we get the browser builds of the @remix-run/web-* packages, which don't work
-    // since our tests run in Node.
+    // because our tests run in Node.
     customExportConditions: ['require'],
   },
   watchPathIgnorePatterns: ['.*\\/node_modules\\/.*', '.*\\/build\\/.*'],
